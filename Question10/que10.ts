0bf15500e8c5e3ec4a20dda479dd7988cf1b74ad
Rubi Abid <160261062+RubiAbid@users.noreply.github.com>
2024-06-20 16:27:25 +0100

    Update que10.ts

--- Question10/que10.ts
+++ Question10/que10.ts
@@ -2,4 +2,17 @@
 // specific to write because your programs are too simple at this point, just add your name and the current date at the top of each
  // program file. Then write one sentence describing what the program does.
 
+// Addition: Adds two numbers together
+console.log(2 + 6);
+
+// Subtraction: Subtracts the second number from the first
+console.log(100 - 92);
+
+// Multiplication: Multiplies two numbers together
+console.log(8 * 1);
+
+// Division: Divides the first number by the second
+console.log(16 / 2);
+
+
  
